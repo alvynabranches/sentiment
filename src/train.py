ca07f0237f37aeb447e360ffa540e9c44f421a7c
Alvyn Abranches <alvynabranches@gmail.com>
2022-06-13 14:56:19 +0530

    Typo fixed

--- src/train.py
+++ src/train.py
@@ -62,7 +62,7 @@ def run():
 
     best_accuracy = 0.832
     for epoch in range(config.START, config.EPOCHS):
-        # engine.train_fn(train_data_loader, model, optimizer, device, scheduler)
+        engine.train_fn(train_data_loader, model, optimizer, device, scheduler)
         outputs, targets = engine.eval_fn(valid_data_loader, model, device)
         outputs = np.array(outputs) >= 0.5
         accuracy = metrics.accuracy_score(targets, outputs)

a50d79b3dd4138ffa909133f9669fedb6601c1f7
Alvyn Abranches <alvynabranches@gmail.com>
2022-06-13 14:20:58 +0530

    Initial Commit

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import transformers
+
+DEVICE = "cuda"
+MAX_LEN = 64
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+START = 2
+EPOCHS = 10
+BERT_PATH = "bert-base-uncased"
+MODEL_PATH = "model.pt"
+TRAINING_FILE = "../input/imdb.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case=True)
